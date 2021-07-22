8afc98ff99b07710f3ac3e548edf871d6a97d784
Galiwango Ananiya <galiwango101@gmail.com>
2021-07-23 00:15:08 +0300

    Implement createBook and removeBook actions.

---
+++ src/actions/index.js
@@ -0,0 +1,7 @@
+export const createBook = ({ id, title, category }) => ({
+  type: 'CREATE_BOOK', payload: { id, title, category },
+});
+
+export const removeBook = ({ id, title, category }) => ({
+  type: 'REMOVE_BOOK', payload: { id, title, category },
+});
