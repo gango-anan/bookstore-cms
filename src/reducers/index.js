02d082951dc7bca11ce3376c590776ce5a2456dd
Galiwango Ananiya <galiwango101@gmail.com>
2021-07-22 19:39:40 +0300

    Refactor code to add functionality to properly generate book ids.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,17 +1,15 @@
 const initialState = {
   books: [
-    { book_id: Date.now().toString, title: 'A Promised Land', category: 'Biography' },
-    { book_id: Date.now().toString, title: 'Deep Learning', category: 'Learning' },
-    { book_id: Date.now().toString, title: 'Wind the Bobbin Up', category: 'Kids' },
-    { book_id: Date.now().toString, title: 'A Game Of Thrones', category: 'Action' },
-    { book_id: Date.now().toString, title: 'The Haunting Of Hill House', category: 'Horror' },
-    { book_id: Date.now().toString, title: 'Long walk To Freedom', category: 'History' },
-    { book_id: Date.now().toString, title: 'The Martian', category: 'Sci-Fi' }
+    { id: Math.random().toString(), title: 'A Promised Land', category: 'Biography' },
+    { id: Math.random().toString(), title: 'Deep Learning', category: 'Learning' },
+    { id: Math.random().toString(), title: 'Wind the Bobbin Up', category: 'Kids' },
+    { id: Math.random().toString(), title: 'A Game Of Thrones', category: 'Action' },
+    { id: Math.random().toString(), title: 'The Haunting Of Hill House', category: 'Horror' },
+    { id: Math.random().toString(), title: 'Long walk To Freedom', category: 'History' },
+    { id: Math.random().toString(), title: 'The Martian', category: 'Sci-Fi' },
   ],
 };
 
-const rootReducer = (state = initialState, action) => {
-  return state;
-};
+const rootReducer = (state = initialState, action = null) => state;
 
 export default rootReducer;
