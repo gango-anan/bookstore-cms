d54b6a8cf85e7596d8452db0a3f466a06e5459b6
Galiwango Ananiya <galiwango101@gmail.com>
2021-07-22 19:42:19 +0300

    Refactor to properly import App component in the root index file.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import App from './components/App';
 import rootReducer from './reducers/index';
 
 const store = createStore(rootReducer);
