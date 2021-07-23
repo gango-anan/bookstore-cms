8bba027d98eadd77976cd65954df9de7996cbaac
Galiwango Ananiya <galiwango101@gmail.com>
2021-07-23 23:19:24 +0300

    Add state to BooksForm component.

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 
-const BooksForm = () => (
-  <form>
-    <div>
-      <label htmlFor="title">
-        ADD NEW BOOK
-        <input id="title" name="title" type="text" placeholder="Add title" />
-      </label>
-    </div>
-    <div>
-      <select id="categories">
-        <option>Category</option>
-        {
+const BooksForm = () => {
+  const [books, setBooks] = useState([{ title: '', category: '' }]);
+  return (
+    <form>
+      <div>
+        <label htmlFor="title">
+          ADD NEW BOOK
+          <input id="title" name="title" type="text" placeholder="Add title" />
+        </label>
+      </div>
+      <div>
+        <select id="categories">
+          <option>Category</option>
+          {
           categories
             .map((category) => (<option key={category} value={category}>{category}</option>))
         }
-      </select>
-    </div>
-    <button type="button">Add book</button>
-  </form>
-);
+        </select>
+      </div>
+      <button type="button">Add book</button>
+    </form>
+  );
+};
 
 export default BooksForm;
