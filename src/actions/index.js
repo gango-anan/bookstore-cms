c7def546993906e91f99d6f321b5a802f082fd04
Galiwango Ananiya <galiwango101@gmail.com>
2021-07-24 19:48:15 +0300

    Create CHANGE_FILTER action.

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,3 +5,7 @@ export const createBook = (id, title, category) => ({
 export const removeBook = (id) => ({
   type: 'REMOVE_BOOK', payload: { id },
 });
+
+export const filter = (name) => ({
+  type: 'CHANGE_FILTER', payload: name,
+});
